b431cb8076f2e2a8dff16ea3bcce27f8f2392056
sonal-bawne <sonalkbawne@gmail.com>
2023-03-11 07:19:58 +0530

    Greeting message for employee Wage problem

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageComputataionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation problem");
+            
+        }
+    }
+}
